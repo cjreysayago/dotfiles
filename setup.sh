ad3300ffd15a3c668f49910cd113e5a3aecbbf1b
cjreysayago <crey@purrfectlabs.com>
2025-02-12 16:42:42 -0300

    fix ohmyzsh plugins installation path

--- setup.sh
+++ setup.sh
@@ -8,8 +8,9 @@ sudo apt update && sudo apt upgrade -y
 
 # Install base software
 echo "Installing base software..."
-sudo apt install -y build-essential tmux curl git unzip zsh-autosuggestions zsh-syntax-highlighting zsh
+sudo apt install build-essential tmux curl git unzip zsh-autosuggestions zsh-syntax-highlighting zsh
 sudo snap install go --classic
+sudo snap install nvim --classic
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
   sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg && \
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | \
@@ -22,24 +23,16 @@ curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
   sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg && \
   sudo apt update && sudo apt install -y 1password-cli
 
-# Install PHP (corrected URL for Linux)
-echo "Installing PHP..."
-sh -c "$(curl -fsSL https://php.new/install/linux)"
-
 # Install OhmyZsh
 echo "Installing ohmyzsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Adding plugins"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-
-# Install NodeJS LTS via nvm
-echo "Installing NodeJS LTS via nvm..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.zshrc
-nvm install --lts
+
+echo "Adding plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom/}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom/}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
 
 # Install JetBrains Nerd Fonts
 echo "Installing JetBrains Nerd Fonts..."
@@ -50,9 +43,19 @@ fc-cache -fv
 # Clone and apply dotfiles
 echo "Applying dotfiles..."
 cp ~/.zshrc ~/.zshrc_bu
-ln -sf ./zsh/.zshrc ~/.zshrc
-ln -sf ./tmux/.tmux.conf ~/.tmux.conf
-ln -sf ./nvim ~/.config/nvim
+ln -sf "$PWD/zsh/.zshrc" ~/.zshrc
+ln -sf "$PWD/tmux/.tmux.conf" ~/.tmux.conf
+ln -sf "$PWD/nvim" ~/.config/nvim
+
+# Install NodeJS LTS via nvm
+echo "Installing NodeJS LTS via nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+source ~/.zshrc
+nvm install --lts
+
+# Install PHP (corrected URL for Linux)
+echo "Installing PHP..."
+/bin/bash -c "$(curl -fsSL https://php.new/install/linux)"
 
 # Final message
 echo "Your environment is ready! Happy shipping! 🚢"
